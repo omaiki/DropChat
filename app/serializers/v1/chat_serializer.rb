c9b4721810eb9929e3265bc1ec9226ddf6bdb9d8
Oshoke Maiki <oshokemaiki@gmail.com>
2017-02-24 00:09:17 -0800

    Add message serializer, update chat serializer

--- app/serializers/v1/chat_serializer.rb
+++ app/serializers/v1/chat_serializer.rb
@@ -3,6 +3,7 @@ module V1
 
     attributes :title, :location, :abstract, :id
     belongs_to :user, serializer: V1::UserSerializer
+    has_many :messages, serializer: V1::MessageSerializer
 
     def abstract
       object.title[0..200]
