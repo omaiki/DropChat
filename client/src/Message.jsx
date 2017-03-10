94a42ff24d7b6d1c518fab587330c4b2c6325ec8
Maquiz <maximilian.bolling@gmail.com>
2017-03-10 00:12:30 -0800

    Imported React Components
    -Installed: react-dom, materialized, and react-tap-event-plugin via npm
    -Linked materialicons, rebofont and materialize css in html head
    -Imported App.jsx, Login and Message into react project
    (message in the List Item component would be a good place to test
    backend to frontend communication)
    Next goal is to separate chat and message components.

---
+++ client/src/Message.jsx
@@ -0,0 +1,120 @@
+import React, { Component } from 'react';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import Avatar from 'material-ui/Avatar';
+import FileFolder from 'material-ui/svg-icons/file/folder';
+import FontIcon from 'material-ui/FontIcon';
+import IconButton from 'material-ui/IconButton';
+import PhotoIcon from 'material-ui/svg-icons/image/photo';
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
+import Divider from 'material-ui/Divider';
+import Infinite from 'react-infinite';
+
+
+import {
+  blue300,
+  indigo900,
+  orange200,
+  deepOrange300,
+  pink400,
+  purple500,
+} from 'material-ui/styles/colors';
+
+const style = {
+  marginLeft: 20,
+};
+
+export default class messages extends Component{
+  render(){
+    return(
+      <div>
+      <Paper zDepth={2}>
+      <Infinite containerHeight={200} elementHeight={40}>
+          <ListItem
+            disabled={true}
+            leftAvatar={
+              <Avatar
+                color={deepOrange300}
+                backgroundColor={purple500}
+                size={30}
+                style={style}
+                >OM </Avatar>
+              }>
+              Whats Good Patna?
+            </ListItem>
+            <Divider />
+            <ListItem
+              disabled={true}
+              rightAvatar={
+                <Avatar
+                  color={deepOrange300}
+                  backgroundColor={blue300}
+                  size={30}
+                  style={style}
+                  >MB </Avatar>
+                }>
+                Nm Chillen Like a Villain
+              </ListItem>
+          <Divider />
+          <ListItem
+            disabled={true}
+            leftAvatar={
+              <Avatar
+                color={deepOrange300}
+                backgroundColor={purple500}
+                size={30}
+                style={style}
+                >OM </Avatar>
+              }>
+              Whats Good Patna?
+            </ListItem>
+            <Divider />
+            <ListItem
+              disabled={true}
+              rightAvatar={
+                <Avatar
+                  color={deepOrange300}
+                  backgroundColor={blue300}
+                  size={30}
+                  style={style}
+                  >MB </Avatar>
+                }>
+                Nm Chillen Like a Villain
+              </ListItem>
+          <Divider />
+          <ListItem
+            disabled={true}
+            leftAvatar={
+              <Avatar
+                color={deepOrange300}
+                backgroundColor={purple500}
+                size={30}
+                style={style}
+                >OM </Avatar>
+              }>
+              Whats Good Patna?
+            </ListItem>
+            <Divider />
+            <ListItem
+              disabled={true}
+              rightAvatar={
+                <Avatar
+                  color={deepOrange300}
+                  backgroundColor={blue300}
+                  size={30}
+                  style={style}
+                  >MB </Avatar>
+                }>
+                Nm Chillen Like a Villain
+              </ListItem>
+          <Divider />
+        </Infinite>
+        <Divider />
+      <TextField hintText="Send Message" name="ChatInput" style={style} underlineShow={false} multiLine={true}  />
+      <IconButton><PhotoIcon /></IconButton>
+      </Paper>
+    </div>
+    )
+  }
+}
