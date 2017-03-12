677585cc48ee2c898828ed625539412eca283b4c
Oshoke Maiki <oshokemaiki@gmail.com>
2017-03-11 17:27:59 -0800

    Add all actions: show,update,edit,destroy for users_controller

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -4,6 +4,12 @@ module V1
 
     # POST /v1/users
     # Creates an user
+
+    def index
+      users = User.all
+      render json: { users: users}
+    end
+
     def create
       @user = User.new user_params
 
@@ -12,6 +18,35 @@ module V1
       else
         render json: { error: ('user_create_error') }, status: :unprocessable_entity
       end
+    end
+
+    def show
+      user = User.find(params[:id])
+      messages = user.messages
+      chats = user.chats
+      render json: { user: user, messages: messages, chats: chats}, status: 200
+    end
+
+    def update
+      user = User.find(params[:id])
+      if user.update_attributes(user_params)
+        render json: { user: user, text: "Profile has been updated successfully"}, status: 200
+      else
+        render json: { error: user.errors.full_messages }, status: 422
+      end
+    end
+
+    def edit
+      user = User.find(params[:id])
+      render json: user, only: [:username, :first_name, :last_name, :info], status: 200
+    end
+
+    def destroy
+      user = User.find(params[:id])
+      if user.destroy
+        render json: {text: 'Account has been deleted successfully'}, status: 200
+      else
+        render json: {error: "Something went wrong, account failed to delete"}, status: 422
     end
 
     private
