c1b1f37c801b66d8a1dcb79d1fa0698dcc8a3349
Oshoke Maiki <oshokemaiki@gmail.com>
2017-03-13 02:10:36 -0700

    Remove FormatDate import to avoid error

--- client/app/bundles/HelloWorld/startup/registration.jsx
+++ client/app/bundles/HelloWorld/startup/registration.jsx
@@ -2,6 +2,7 @@ import ReactOnRails from 'react-on-rails';
 
 import HelloWorld from '../components/HelloWorld';
 import ShowChats from '../components/chats';
+import Appointments from '../components/appointments';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
