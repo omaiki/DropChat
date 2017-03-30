a9a159c506d83f8e45ba096e859b26a55c58935b
Oshoke Maiki <oshokemaiki@gmail.com>
2017-03-30 00:16:39 -0700

    Add firebase to app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
+
+ var config = {
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
+  };
+
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
