7f598db261f421898918aed5cbdf160b0aff3b4e
Oshoke Maiki <oshokemaiki@gmail.com>
2017-03-31 02:33:16 -0700

    Add signup component

---
+++ src/components/Signup.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import * as firebase from 'firebase';
+
+class Signup extends Component {
+  render(){
+    return <div> Signup</div>;
+  }
+
+}
+
+export default Signup;
