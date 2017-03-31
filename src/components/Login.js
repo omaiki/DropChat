f290978964e35e42e83d195325f0034bc2be99f2
Oshoke Maiki <oshokemaiki@gmail.com>
2017-03-31 02:36:56 -0700

    Add login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,2 +1,10 @@
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
+
+class Login extends Component {
+  render(){
+    return <div> Login </div>;
+  }
+}
+
+export default Login;
