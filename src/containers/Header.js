d2fc41918d0910d44eceae336f92f9f001630749
Oshoke Maiki <oshokemaiki@gmail.com>
2017-03-31 23:48:23 -0700

    Add react-router links to nav bar for dynamic loading

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,25 +1,32 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import * as firebase from 'firebase';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class Header extends Component {
   render(){
     return (
+    <Router>
       <nav className="navbar navbar-default">
        <div className="container-fluid">
          <div className="navbar-header">
-           <a href="/" className="navbar-brand">DropChat</a>
+           <Link className="navbar-brand" to="/">DropChat</Link>
          </div>
           <ul className="nav navbar-nav navbar-right">
             <li className="nav-item">
-              <a className="nav-link" href="/login">Login</a>
+              <Link className="nav-link" to="/login">Login</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/signup">Sign Up</a>
+              <Link className="nav-link" to="/signup">Sign Up</Link>
             </li>
           </ul>
        </div>
      </nav>
+     </Router>
    );
   }
 }
