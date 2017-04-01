e9f68d216519fb916bda3ad5f47b95e5cdb88fb9
RMC1 <laloatcal@yahoo.com>
2017-03-31 18:52:50 -0700

    Resolve merge conflicts

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Chat from './components/Chat.js'
+
+
+class App extends Component {
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+
+        <Chat />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import * as firebase from 'firebase';
+
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBOtUift82Bq8CFIklt-kbRx5QFjcb8P2g",
+    authDomain: "refire-app.firebaseapp.com",
+    databaseURL: "https://refire-app.firebaseio.com",
+    storageBucket: "refire-app.appspot.com",
+    messagingSenderId: "201898147893"
+  };
+
+  firebase.initializeApp(config);
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
