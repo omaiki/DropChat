ab7ccfe06f1a07709cb0185602f770ce29f419b1
Oshoke Maiki <oshokemaiki@gmail.com>
2017-04-03 00:57:49 -0700

    Add Google Auth with simple sign in and sign out

--- src/Application.js
+++ src/Application.js
@@ -9,16 +9,33 @@ import './Application.css';
 class Application extends Component {
   constructor(props) {
     super(props);
+
+    //not logged in by default
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  //when component mount
+  componentDidMount() {
+    auth.onAuthStateChanged((currentUser) => {
+      console.log('AUTH_CHANGE', currentUser);
+      this.setState({ currentUser});
+    });
+
   }
 
   render() {
+    const { currentUser } = this.state;
+
     return (
       <div className="Application">
         <header className="Application--header">
-          <h1>Lunch Rush</h1>
+          <h1>DropChat</h1>
         </header>
         <div>
-        <SignIn />
+        {!currentUser && <SignIn />}
+        {currentUser && <CurrentUser user={currentUser} />}
         </div>
       </div>
     );
--- src/CurrentUser.js
+++ src/CurrentUser.js
@@ -5,7 +5,20 @@ import './CurrentUser.css';
 const CurrentUser = ({ user }) => {
   return (
     <div className="CurrentUser">
+       <img
+       className="CurrentUser--photo"
+          src={ user.photoURL }
+          alt={ user.displayName }
+       />
+       <div className="CurrentUser--identification">
+       <h3> {user.displayName } </h3>
+       <p> { user.email } </p>
+       <button onClick={() => auth.signOut()}>
+          Sign Out
+       </button>
+       </div>
     </div>
+
   );
 };
 
