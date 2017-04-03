85f689331f76e6ea764eb96cbf9e98775a6f1143
Oshoke Maiki <oshokemaiki@gmail.com>
2017-04-03 01:22:50 -0700

    Add button for logging in with Facebook

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { auth, googleAuthProvider } from './firebase';
+import { auth, googleAuthProvider, facebookAuthProvider } from './firebase';
 
 class SignIn extends Component {
   render() {
     return (
       <div className="SignIn">
       <button onClick={() => auth.signInWithPopup(googleAuthProvider)}>
-      Sign In
+      Login with Google Account
+      </button>
+      <button onClick={() => auth.signInWithPopup(facebookAuthProvider)}>
+      Login with Facebook Account
       </button>
       </div>
     );
