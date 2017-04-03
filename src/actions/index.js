0b87d719dc3771bbf5d4d7a445f0848f629a3f9a
Oshoke Maiki <oshokemaiki@gmail.com>
2017-04-02 17:41:06 -0700

    More redux actions for auth flow

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,6 @@ import Firebase from 'firebase';
 export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 export const REQUEST_GIFS = 'REQUEST_GIFS';
-export const SIGN_IN_USER = 'SIGN_IN_USER';
 export const SIGN_OUT_USER = 'SIGN_OUT_USER';
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const AUTH_USER = 'AUTH_USER';
@@ -14,12 +13,13 @@ const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
 const API_KEY = '&api_key=dc6zaTOxFJmzC';
 
 const config = {
-  apiKey: "AIzaSyBOtUift82Bq8CFIklt-kbRx5QFjcb8P2g",
-  authDomain: "refire-app.firebaseapp.com",
-  databaseURL: "https://refire-app.firebaseio.com",
-  storageBucket: "refire-app.appspot.com",
-  messagingSenderId: "201898147893"
+
+    apiKey: "AIzaSyBOtUift82Bq8CFIklt-kbRx5QFjcb8P2g",
+    authDomain: "refire-app.firebaseapp.com",
+    databaseURL: "https://refire-app.firebaseio.com",
 };
+
+Firebase.initializeApp(config);
 
 export function requestGifs(term = null) {
   return function(dispatch) {
@@ -31,7 +31,6 @@ export function requestGifs(term = null) {
     });
   }
 }
-
 export function openModal(gif) {
   return {
     type: OPEN_MODAL,
@@ -45,21 +44,50 @@ export function closeModal() {
   }
 }
 
-export function signInUser()
-{
-  browserHistory.push('/favorites');
-
-  return {
-    type: SIGN_IN_USER
+export function signUpUser(credentials) {
+  return function(dispatch) {
+    Firebase.auth().createUserWithEmailAndPassword(credentials.email, credentials.password)
+      .then(response => {
+        dispatch(authUser());
+        browserHistory.push('/favorites');
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch(authError(error));
+      });
   }
 }
 
+export function signInUser(credentials) {
+  return function(dispatch) {
+    Firebase.auth().signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then(response => {
+        dispatch(authUser());
+        browserHistory.push('/favorites');
+      })
+      .catch(error => {
+        dispatch(authError(error));
+      });
+  }
+}
 
-export function signOutUser()
-{
+export function signOutUser() {
   browserHistory.push('/');
 
   return {
     type: SIGN_OUT_USER
+  }
+}
+
+export function authUser() {
+  return {
+    type: AUTH_USER
+  }
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
   }
 }
