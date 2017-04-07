5015127d6b510d7feb6daa466d1b4d1252a891ea
Oshoke Maiki <oshokemaiki@gmail.com>
2017-04-07 13:36:18 -0700

    List name of voters for restaurant votes

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -4,10 +4,13 @@ import './Restaurant.css';
 
 class Restaurant extends Component {
   render () {
-    const { name, handleSelect, handleDeselect } = this.props;
+    const { name, votes, handleSelect, handleDeselect } = this.props;
     return (
       <article className="Restaurant">
       <h3> { name } </h3>
+      <ul>
+        {votes && map(votes, (vote, key) => <li key={key}>{ vote }</li>)}
+      </ul>
       <button onClick={handleSelect}>
         Join
       </button>
