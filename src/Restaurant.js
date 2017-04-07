5295af330daebf333b36b62f6015fdc7a9f03dd3
Oshoke Maiki <oshokemaiki@gmail.com>
2017-04-07 13:43:35 -0700

    Polish experience: use restaurant reference for chat creation, allow users to join chat

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -4,19 +4,26 @@ import './Restaurant.css';
 
 class Restaurant extends Component {
   render () {
-    const { name, votes, handleSelect, handleDeselect } = this.props;
+    const { name, user, votes, handleSelect, handleDeselect } = this.props;
+    const userHasSelected = votes &&  Object.keys(votes).includes(user.uid)
+
     return (
       <article className="Restaurant">
       <h3> { name } </h3>
       <ul>
         {votes && map(votes, (vote, key) => <li key={key}>{ vote }</li>)}
       </ul>
-      <button onClick={handleSelect}>
-        Join
-      </button>
-      <button className="destructive" onClick={handleDeselect}>
-        Leave
-      </button>
+      {
+        userHasSelected
+        ? <button className="destructive" onClick={handleDeselect}>
+          Leave
+        </button>
+        :
+        <button onClick={handleSelect}>
+          Join
+        </button>
+
+      }
       </article>
     );
   }
--- src/Restaurants.js
+++ src/Restaurants.js
@@ -30,7 +30,7 @@ class Restaurants extends Component {
 
 
   render () {
-    const { restaurants } = this.props;
+    const { user, restaurants } = this.props;
     return (
       <section className="Restaurants">
         {
@@ -38,6 +38,7 @@ class Restaurants extends Component {
               return <Restaurant
                 key={key}
                 {...restaurant}
+                user={user}
                 handleSelect={() => this.handleSelect(key)}
                 handleDeselect={() => this.handleDeselect(key)}
               />;
